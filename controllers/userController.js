c9bcd03329c236f5a068508ad69519ff7b176032
sukmabrahmantya <brahmantyasukma@gmail.com>
2019-11-28 19:11:27 +0700

    structure login

---
+++ controllers/userController.js
@@ -0,0 +1,19 @@
+'use strict'
+
+class userController {
+
+  static signIn(req, res, next) {
+
+  }
+
+  static signInGoogle(req, res, next) {
+
+  }
+
+  static register(req, res, next) {
+
+  }
+
+}
+
+module.exports = userController
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router()
 const zomatoRouter = require('./routesZomato');
+const userRouter = require('./userRouter')
 
 router.use("/api/zomato", zomatoRouter);
+router.use('/user', userRouter)
 
 module.exports = router
